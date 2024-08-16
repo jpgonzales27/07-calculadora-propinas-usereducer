b93fdcb743457ba0abc70588617f46a67ba0913a
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-16 17:05:46 -0400

    Agrendo taildwind para nuestro menu

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -6,9 +6,9 @@ type MenuItemProps = {
 
 export default function MenuItem({ item }: MenuItemProps) {
   return (
-    <>
+    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between rounded-lg">
       <p>{item.name}</p>
-      <p>{item.price}</p>
-    </>
+      <p className="font-black">${item.price}</p>
+    </button>
   );
 }
