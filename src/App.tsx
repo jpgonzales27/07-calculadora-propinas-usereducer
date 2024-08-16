5912543fa1304f9d819d5a53814a555a6807977b
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-16 16:54:43 -0400

    creando type y rendirizando la data en la ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db";
+import type { MenuItem as MenuItemType } from "./types";
+
 function App() {
+  const [data, setData] = useState<MenuItemType[]>([]);
+
+  useEffect(() => {
+    setData(menuItems);
+  }, []);
+
   return (
     <>
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de propinas y Consumo</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+          {data.map((item) => (
+            <MenuItem key={item.id} item={item} />
+          ))}
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,14 @@
+import type { MenuItem } from "../types";
+
+type MenuItemProps = {
+  item: MenuItem;
+};
+
+export default function MenuItem({ item }: MenuItemProps) {
+  return (
+    <>
+      <p>{item.name}</p>
+      <p>{item.price}</p>
+    </>
+  );
+}
