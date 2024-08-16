3e3fd16d600a5a6e874ef9bfc681e0ccab7a8c6e
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-16 17:48:03 -0400

    funciona agregar orden sin duplicados

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,7 +5,19 @@ export default function useOrder() {
   const [order, setOrder] = useState<OrdenItem[]>([]);
 
   const addItem = (item: MenuItem) => {
-    console.log("Agregando...", item);
+    const itemExist = order.find((orderItem) => orderItem.id === item.id);
+    if (itemExist) {
+      const updateOrder = order.map((orderItem) =>
+        orderItem.id === item.id ? { ...orderItem, quantity: orderItem.quantity + 1 } : orderItem
+      );
+      setOrder(updateOrder);
+    } else {
+      const newItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
+    }
   };
+
+  console.log(order);
+
   return { addItem };
 }
